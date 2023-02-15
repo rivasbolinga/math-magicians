ec5cc27bdc04c750822b1187bc5781e3b4d21814
Andrea <andrearivas4cinfor@gmail.com>
2023-02-15 12:37:13 +0100

    Refactoring

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,49 +1,51 @@
-// import calculate from './logic/calculate.js'
 import React, { useState } from 'react';
-import Input from './input.js';
-import Button from './buttons.js'
-const Calculator = () => {
-  // -- Need to destructure the array because useState always
-  //returns array wit 2 values.
-  const [result, setResult] = useState('');
-  const [first, setFirst] = useState('');
-  //Take the strinc of every button to display it in the calculator screen
-  const addInput = (e) => {
-    setFirst((first) =>[...first, e + " "])
-    
-  }
+import calculate from './logic/calculate';
+
+function Calculator() {
+  const [result, setCalculatorData] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+
+  const handleClick = (buttonName) => {
+    setCalculatorData(calculate(result, buttonName));
+  };
+
   return (
-  <div className="calculadora">
-  <Input first = {first} result= {result}/>
+    <div className='calculadora'>
+    <div className='display-screen-container'>
+      <input className='display-screen' type="text" value={result.next || result.total || '0'}  />
+      </div>
 
-  <div className="all-btns">
-  <div className="buttons">
-    <Button name='AC' />
-    <Button name='+/-'handleClick={addInput} />
-    <Button name='%' handleClick={addInput} />
-    <Button name="7"handleClick={addInput} />
-    <Button name="8" handleClick={addInput} />
-    <Button name='9' handleClick={addInput}/>
-    <Button name='4' handleClick={addInput}/>
-    <Button name='5'handleClick={addInput} />
-    <Button name='6' handleClick={addInput}/>
-    <Button name='1' handleClick={addInput}/>
-    <Button name='2' handleClick={addInput}/>
-    <Button name='3' handleClick={addInput}/>
-    <Button name='0' handleClick={addInput}/>
-    <Button name='.' handleClick={addInput}/>
+      <div className='all-btns'>
+      <div className='buttons'>
+        <button className='button' onClick={() => handleClick('AC')}>AC</button>
+        <button className='button' onClick={() => handleClick('+/-')}>+/-</button>
+        <button className='button' onClick={() => handleClick('%')}>%</button>
+        <button className='button' onClick={() => handleClick('7')}>7</button>
+        <button className='button' onClick={() => handleClick('8')}>8</button>
+        <button className='button' onClick={() => handleClick('9')}>9</button>
+        <button className='button' onClick={() => handleClick('4')}>4</button>
+        <button className='button' onClick={() => handleClick('5')}>5</button>
+        <button className='button' onClick={() => handleClick('6')}>6</button>
+        <button className='button' onClick={() => handleClick('1')}>1</button>
+        <button className='button' onClick={() => handleClick('2')}>2</button>
+        <button className='button' onClick={() => handleClick('3')}>3</button>
+        <button className='button zero' onClick={() => handleClick('0')}>0</button>
+        <button className='button' onClick={() => handleClick('.')}>.</button>
+        </div>
+      <div className="operations">
+       <button className="ope divide" onClick={() => handleClick('÷')}>÷</button>
+       <button className="ope por" onClick={() => handleClick('x')}>x</button>
+       <button className="ope minus" onClick={() => handleClick('-')}>-</button>
+       <button className="ope sum"onClick={() => handleClick('+')}>+</button>
+       <button className="equal"onClick={() => handleClick('=')}>=</button>
     </div>
-  <div className="operations">
-       <Button name='÷' />
-       <Button name='x' />
-       <Button name='-' />
-       <Button name='+' />
-       <Button name='=' />
     </div>
-
-</div>
-</div>
+    </div>
   );
 }
+
 
 export default Calculator;
