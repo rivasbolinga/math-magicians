eaee339084a1d45de112296b1eee13c39721ef1f
Andrea <andrearivas4cinfor@gmail.com>
2023-02-20 10:28:03 +0100

    Fix linters

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import Calculator from './components/Calculator.js';
-import './App.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import  Home  from './components/Home.js';
 import Quote from './components/Quote';
