bb1ea49f911b1ee2f521ceaeeb9d37ebdac319a8
Andrea <andrearivas4cinfor@gmail.com>
2023-02-20 07:58:07 +0100

    Add functionality to open and close menu

---
+++ src/components/CloseButton.js
@@ -0,0 +1,6 @@
+import './CloseButton.scss'
+const CloseButton = () => {
+  <div className="close-btn">x</div>
+}
+
+export default CloseButton;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,49 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import './Navbar.scss';
 import Hamburger from './Hamburger';
+import CloseButton from './CloseButton';
+import React, { useState } from 'react';
 const Navbar = () => {
-  return(
-<nav className="navbar">
+  const [hamburgerOpen, setHamburgerOpen] = useState(false);
+  const [showCloseButton, setShowCloseButton] = useState(false);
 
-  <div className='container-nav'>
-    <a className='navbar-title'>Math Magicians</a>
-    <div className='hamburger-container'>
-     <Hamburger />
-    </div>
-    <ul className='navbar-nav'>
-      <li className='navbar-item'>
-        <Link to="/" className='navbar-link'>Home</Link>
-        </li>
-        <li className='navbar-item'>
-        <Link to="/Calculator" className='navbar-link'>Calculator</Link>
-        </li>
-        <li className='navbar-item'>
-        <Link to="/Quote" className='navbar-link'>Quote</Link>
-        </li>      
-    </ul>
-  </div>
-</nav>
+  const toggleHamburger = () => {
+    setHamburgerOpen(!hamburgerOpen);
+    if (!hamburgerOpen) {
+      setShowCloseButton(true);
+    }
+  }
+  
+  const handleClose = () => {
+    setHamburgerOpen(false);
+    setShowCloseButton(false);
+  }
+
+  const navBarClass = `navbar-nav ${hamburgerOpen ? 'open' : ''}`;
+
+  return (
+    <nav className="navbar">
+      <div className='container-nav'>
+        <a className='navbar-title'>Math Magicians</a>
+        <div className='hamburger-container' onClick={toggleHamburger}>
+          {showCloseButton ? <CloseButton onClick={handleClose}/> : <Hamburger />}
+        </div>
+        <ul className={navBarClass}>
+          <li className='navbar-item'>
+            <Link to="/" className='navbar-link'>Home</Link>
+          </li>
+          <li className='navbar-item'>
+            <Link to="/Calculator" className='navbar-link'>Calculator</Link>
+          </li>
+          <li className='navbar-item'>
+            <Link to="/Quote" className='navbar-link'>Quote</Link>
+          </li>      
+        </ul>
+      </div>
+    </nav>
   );
 };
 
 export default Navbar;
+
+
